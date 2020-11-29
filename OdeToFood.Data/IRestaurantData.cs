62a03c54c07c898fc79126bd09e7d19e232cb86a
simonojok19 <simonojok19@gmail.com>
2020-11-30 01:12:04 +0300

    changed the page search

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -9,7 +9,7 @@ namespace OdeToFood.Data
 
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantByName(string name);
     }
 
     public class InMemoryRestuarantData : IRestaurantData
@@ -44,9 +44,10 @@ namespace OdeToFood.Data
                 }
             };
         }
-        public IEnumerable<Restaurant> GetAll()
+        public IEnumerable<Restaurant> GetRestaurantByName(string name = null)
         {
             return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
 
