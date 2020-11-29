36f9eeb9a222e200c3a11b269ad069a897ab6105
simonojok19 <simonojok19@gmail.com>
2020-11-29 20:46:36 +0300

    added a new enum

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -2,6 +2,14 @@
 
 namespace OdeToFood.Core
 {
+    public enum CuisineType
+    {
+        None,
+        Mexican,
+        Italian,
+        Indian
+    }
+
     public class Restaurant
     {
         public int Id { get; set; }
