a4bc267f4281f222e2643fa6ca9a854e6ff70609
simonojok19 <simonojok19@gmail.com>
2020-11-29 20:45:01 +0300

    added the class for restaurant

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -4,5 +4,9 @@ namespace OdeToFood.Core
 {
     public class Restaurant
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
     }
 }
