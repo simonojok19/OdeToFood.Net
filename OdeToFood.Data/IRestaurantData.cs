2c0643133a0399951c564de392869a1da1e4ceb2
simonojok19 <simonojok19@gmail.com>
2020-11-29 20:56:17 +0300

    added reference to ode to food

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using OdeToFood.Core;
+using System;
+using System.Collections;
 
 namespace OdeToFood.Data
 {
 
     public interface IRestaurantData
     {
+        IEnumerable<Restaurant>
     }
 }
