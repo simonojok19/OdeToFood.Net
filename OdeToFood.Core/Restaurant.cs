0b6df45badc3e9f7fa8d9b216e6bf106d5e03bea
simonojok19 <simonojok19@gmail.com>
2020-11-29 20:49:02 +0300

    finished restaurants

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -8,6 +8,6 @@ namespace OdeToFood.Core
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
-
+        public CuisineType Cuisine { get; set; }
     }
 }
