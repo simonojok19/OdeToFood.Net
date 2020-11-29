4fb0a110c0eb79afb6723d851ab73e78444a2bb0
simonojok19 <simonojok19@gmail.com>
2020-11-29 20:41:10 +0300

    added a new project

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood.Pages.Restaurants
+{
+
+    public class ListModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public ListModel()
+        {
+
+        }
+        public void OnGet()
+        {
+            Message = "Hello World Simon Peter";
+        }
+    }
+}
