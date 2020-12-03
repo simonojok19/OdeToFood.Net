1bf602f2fa0f4ec2559327d13692c74c2d432ba7
simonojok19 <simonojok19@gmail.com>
2020-12-03 09:38:15 +0300

    adding some html contents

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
-    public class IndexModel : PageModel
+    public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IRestaurantData restaurantData;
+        public Restaurant Restaurant { get; set; }
+        public EditModel(IRestaurantData restaurantData)
         {
+            this.restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
         }
     }
 }
