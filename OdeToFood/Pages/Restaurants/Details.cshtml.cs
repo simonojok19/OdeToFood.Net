68faab3bb51829fbf562196605cb71aaf02ac24b
simonojok19 <simonojok19@gmail.com>
2020-12-03 08:41:56 +0300

    added another asp page

--- OdeToFood/Pages/Restaurants/Details.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Details.cshtml.cs
@@ -5,16 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class DetailsModel : PageModel
     {
         public Restaurant Restaurant { get; set; }
-        public void OnGet(int restaurantId)
+        private readonly IRestaurantData restaurantData;
+
+        public DetailsModel(IRestaurantData restaurantData)
         {
-            Restaurant = new Restaurant();
-            Restaurant.Id = restaurantId;
+            this.restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
         }
     }
 }
